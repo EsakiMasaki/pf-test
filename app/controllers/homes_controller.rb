0f4463727e6c2f9a8c8da8742dad6774fea94681
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2023-01-01 13:07:27 +0000

    category

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,11 @@
 class HomesController < ApplicationController
   def top
     @notes = Note.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
+    @notes.each do |note|
+      unless Category.exists?(id: note.category_id)
+        note.category_id = nil
+      end
+    end
   end
 
   def about
